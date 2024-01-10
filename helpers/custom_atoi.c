140d7cd4c9210e01698e16b98ca8683448468144
Ismail Jaija <ijaija@e1r3p5.1337.ma>
2024-01-10 10:18:09 +0100

    hmm

--- helpers/custom_atoi.c
+++ helpers/custom_atoi.c
@@ -6,13 +6,13 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 10:17:47 by ijaija            #+#    #+#             */
-/*   Updated: 2024/01/09 11:11:40 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/01/10 10:16:36 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/helpers.h"
 
-static long	part2(int i, int sign, const char *str)
+static long	part2(int i, int sign, const char *str, int passed)
 {
 	int		newi;
 	long	res;
@@ -30,6 +30,8 @@ static long	part2(int i, int sign, const char *str)
 	}
 	if (str[newi] && !ft_isdigit(str[newi]))
 		return (9999999999999);
+	if (passed == 1 && res == 0)
+		return (0);
 	if (sign == 0)
 		return (res);
 	if (sign != 0 && res == 0)
@@ -41,9 +43,11 @@ long	custom_atoi(const char *str)
 {
 	int	i;
 	int	sign;
+	int	passed;
 
 	i = 0;
 	sign = 0;
+	passed = 0;
 	while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
 			|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f'))
 		i++;
@@ -53,7 +57,9 @@ long	custom_atoi(const char *str)
 			sign = -1;
 		else if (str[i] == '+')
 			sign = 1;
+		if (ft_isdigit(str[i + 1]))
+			passed = 1;
 		i++;
 	}
-	return (part2(i, sign, str));
+	return (part2(i, sign, str, passed));
 }
