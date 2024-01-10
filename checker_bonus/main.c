b79aded9ce1464a53035c0e3a02cdb1541693dd4
Ismail Jaija <ijaija@e1r3p5.1337.ma>
2024-01-10 10:04:15 +0100

    hmm

--- checker_bonus/main.c
+++ checker_bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 15:08:39 by ijaija            #+#    #+#             */
-/*   Updated: 2023/12/31 19:51:38 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/01/10 10:00:40 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ int	main(int argc, char **argv)
 	if (args_parsing(&argc, &argv, &args) == -1)
 		fail_exit();
 	s_a = array_to_list(args, argc);
-	if (check_sorted(s_a, argc) == 1)
-		return (freeingx(argv), free(args), l_free(&s_a), ft_printf("OK\n"));
 	file = reading_input();
+	if (check_sorted(s_a, argc) == 1 && ft_split(file, '\n') == NULL)
+		return (freeingx(argv), free(args), l_free(&s_a), ft_printf("OK\n"));
 	if (instructions_check(file) == -1)
 		fail_exit();
 	instructions = ft_split(file, '\n');
